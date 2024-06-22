ddfc6bbb394c85e04131fc298428f232a7452624
One290 <lenya.dorovskoy@mail.ru>
2024-06-22 20:02:29 +0800

    dtjfklkk

---
+++ Web/ClientApplication/src/ts/site.ts
@@ -0,0 +1,14 @@
+import 'jquery';
+import 'jquery-validation';
+import 'jquery-validation-unobtrusive';
+import 'bootstrap';
+import 'bootstrap-table'
+import '@fortawesome/fontawesome-free/js/all.min';
+
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap-table/dist/bootstrap-table.min.css';
+import '@fortawesome/fontawesome-free/css/all.min.css';
+
+import '../css/site.css';
+
+console.log('The \'site\' bundle has been loaded!');
---
+++ Web/ClientApplication/webpack.config.js
@@ -0,0 +1,39 @@
+const path = require('path');
+
+module.exports = {
+    entry: {
+        site: './src/ts/site.ts'
+    },
+    output: {
+        filename: '[name].entry.js',
+        path: path.resolve(__dirname, '..', 'wwwroot', 'dist'),
+        clean: true
+    },
+    devtool: 'source-map',
+    mode: 'development',
+    resolve: {
+        extensions: [".ts", ".js"],
+        extensionAlias: {'.js': ['.js', '.ts']}
+    },
+    module: {
+        rules: [
+            {
+                test: /\.ts$/i,
+                use: ['ts-loader'],
+                exclude: /node_modules/
+            },
+            {
+                test: /\.css$/,
+                use: ['style-loader', 'css-loader']
+            },
+            {
+                test: /\.(png|svg|jpg|jpeg|gif|webp)$/i,
+                type: 'asset'
+            },
+            {
+                test: /\.(eot|woff(2)?|ttf|otf|svg)$/i,
+                type: 'asset'
+            }
+        ]
+    }
+};
