db2e7cd1fe393beedd6e1b43719a798446345df9
One290 <lenya.dorovskoy@mail.ru>
2024-07-07 19:01:32 +0800

    brought back the frontend

---
+++ Source/Web/ClientApplication/src/ts/site.ts
@@ -0,0 +1,9 @@
+import 'bootstrap';
+import 'bootstrap-table'
+import '@fortawesome/fontawesome-free/js/all.min';
+
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap-table/dist/bootstrap-table.min.css';
+import '@fortawesome/fontawesome-free/css/all.min.css';
+
+import '../css/site.css';
---
+++ Source/Web/ClientApplication/webpack.config.js
@@ -0,0 +1,46 @@
+const path = require('path');
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+
+module.exports = {
+    entry: {
+        site: './src/ts/site.ts',
+        validation: './src/ts/validation.ts'
+    },
+    output: {
+        filename: '[name].entry.js',
+        path: path.resolve(__dirname, '..', 'wwwroot', 'dist'),
+        clean: true
+    },
+    devtool: 'source-map',
+    mode: 'development',
+    resolve: {
+        extensions: [".ts", ".js"],
+        extensionAlias: {'.js': ['.js', '.ts']}
+    },
+    module: {
+        rules: [
+            {
+                test: /\.ts$/i,
+                use: ['ts-loader'],
+                exclude: /node_modules/
+            },
+            {
+                test: /\.css$/,
+                use: [{ loader: MiniCssExtractPlugin.loader }, 'css-loader']
+            },
+            {
+                test: /\.(png|svg|jpg|jpeg|gif|webp)$/i,
+                type: 'asset'
+            },
+            {
+                test: /\.(eot|woff(2)?|ttf|otf|svg)$/i,
+                type: 'asset'
+            }
+        ]
+    },
+    plugins: [
+        new MiniCssExtractPlugin({
+            filename: "[name].css"
+        })
+    ]
+};
