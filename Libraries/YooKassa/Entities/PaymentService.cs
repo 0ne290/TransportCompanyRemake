65fa0f3c072a99c6415a0dd8b3e7fcfa45f42019
Алексей <97665299+0ne290@users.noreply.github.com>
2024-07-24 23:10:33 +0800

    Update and rename YooKassa.cs to PaymentService.cs

---
+++ Libraries/YooKassa/Entities/PaymentService.cs
@@ -0,0 +1,19 @@
+﻿using YooKassa.Entities.Payment;
+
+namespace YooKassa.Entities;
+
+// Все-таки лучше сделать Confirmation одним фиксированным классом, если это возможно
+public class PaymentService
+{
+    public PaymentService(IHttpClientFactory httpClientFactory)
+    {
+        _httpClient = httpClientFactory.CreateClient("YooKassaPaymentApi");
+    }
+
+    public IssuedPayment CreatePayment(UnissuedPayment unissuedPayment)
+    {
+        
+    }
+    
+    private readonly HttpClient _httpClient;
+}
