9a239e34ddbf1ef389e9e8d8f5843f4128a4286a
Алексей <97665299+0ne290@users.noreply.github.com>
2024-08-17 00:31:51 +0800

    Update UpdateRequest.cs

--- Source/Core/Application/Dtos/User/UpdateRequest.cs
+++ Source/Core/Application/Dtos/User/UpdateRequest.cs
@@ -8,67 +8,67 @@ public class UpdateRequest : PropertiesSetFactCheckBase
     [JsonProperty(Required = Required.Always)]
     public required string Guid { get; init; }
     
-    [DefaultValue("")]
+    [DefaultValue(null)]
     [JsonProperty(DefaultValueHandling = DefaultValueHandling.Populate)]
-    public required string SetLogin
+    public required string? SetLogin
     {
         get => _setLogin;
         init
         {
-            if (value != "")
+            if (value != null)
                 SetProperty(nameof(SetLogin));
 
             _setLogin = value;
         }
     }
     
-    [DefaultValue("")]
+    [DefaultValue(null)]
     [JsonProperty(DefaultValueHandling = DefaultValueHandling.Populate)]
-    public required string SetPassword
+    public required string? SetPassword
     {
         get => _setPassword;
         init
         {
-            if (value != "")
+            if (value != null)
                 SetProperty(nameof(SetPassword));
 
             _setPassword = value;
         }
     }
     
-    [DefaultValue("")]
+    [DefaultValue(null)]
     [JsonProperty(DefaultValueHandling = DefaultValueHandling.Populate)]
-    public required string SetName
+    public required string? SetName
     {
         get => _setName;
         init
         {
-            if (value != "")
+            if (value != null)
                 SetProperty(nameof(SetName));
 
             _setName = value;
         }
     }
     
-    [DefaultValue("")]
+    [DefaultValue(null)]
     [JsonProperty(DefaultValueHandling = DefaultValueHandling.Populate)]
-    public required string SetContact
+    public required string? SetContact
     {
         get => _setContact;
         init
         {
-            if (value != "")
+            if (value != null)
                 SetProperty(nameof(SetContact));
 
             _setContact = value;
         }
     }
     
-    private readonly string _setLogin = null!;
+    private readonly string? _setLogin;
     
-    private readonly string _setPassword = null!;
+    private readonly string? _setPassword;
     
-    private readonly string _setName = null!;
+    private readonly string? _setName;
     
-    private readonly string _setContact = null!;
+    private readonly string? _setContact;
 }
