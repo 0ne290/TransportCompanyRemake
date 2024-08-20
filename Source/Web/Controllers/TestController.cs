a98c1b535605c1c02e33de52e45da9c0ec5c6b5a
One290 <lenya.dorovskoy@mail.ru>
2024-08-20 15:29:06 +0800

    dnkhdy

--- Source/Web/Controllers/TestController.cs
+++ Source/Web/Controllers/TestController.cs
@@ -15,63 +15,52 @@ public class TestController(TransportCompanyContext dbContext, Administrator adm
     {
         await TransportCompanyContext.LoadTestData(dbContext);
     }
-    
+
     [Route("clear-database")]
     public async Task Cxz()
     {
         await dbContext.Database.EnsureDeletedAsync();
         await dbContext.Database.EnsureCreatedAsync();
     }
-    
+
     [HttpPost]
-    [Route("create-drivers")]
-    public async Task CreateDrivers([FromBody]IReadOnlyCollection<Application.Dtos.Driver.CreateRequest> createRequests)
+    [Route("drivers")]
+    public async Task CreateDrivers(
+        [FromBody] IReadOnlyCollection<Application.Dtos.Driver.CreateRequest> createRequests)
     {
         await administrator.CreateDrivers(createRequests);
     }
-    
+
     [HttpPatch]
     [Route("drivers")]
-    public async Task UpdateDrivers([FromBody]IReadOnlyCollection<Application.Dtos.Driver.UpdateRequest> updateRequests)
+    public async Task UpdateDrivers(
+        [FromBody] IReadOnlyCollection<Application.Dtos.Driver.UpdateRequest> updateRequests)
     {
         Console.WriteLine(JsonConvert.SerializeObject(updateRequests));
         await administrator.UpdateDrivers(updateRequests);
     }
-    
-    [HttpPatch]
-    [Route("trucks")]
-    public async Task UpdateTrucks([FromBody]IReadOnlyCollection<Application.Dtos.Truck.UpdateRequest> updateRequests)
-    {
-        Console.WriteLine(JsonConvert.SerializeObject(updateRequests));
-        await administrator.UpdateTrucks(updateRequests);
-    }
-    
-    [HttpPost]
-    [Route("branches")]
-    public async Task CreateBranches([FromBody]IReadOnlyCollection<Application.Dtos.Branch.CreateRequest> createRequests)
-    {
-        await administrator.CreateBranches(createRequests);
-    }
-    
-    [HttpGet]
-    [Route("branches")]
-    public async Task<IActionResult> GetBranches()
-    {
-        return View("Branches", (await administrator.GetBranches(FilterParser.Parse<Branch>("true"), true, true)).Select(b => new ViewModels.Branch(b)).ToList());
-    }
-    
-    [HttpDelete]
-    [Route("branches")]
-    public async Task DeleteBranches(string filter)
-    {
-        await administrator.DeleteBranches(FilterParser.Parse<Branch>(filter));
-    }
-    
+
     [HttpDelete]
     [Route("drivers")]
     public async Task DeleteDrivers(string filter)
     {
         await administrator.DeleteDrivers(FilterParser.Parse<Driver>(filter));
+    }
+    
+    [HttpPost]
+    [Route("trucks")]
+    public async Task CreateTrucks(
+        [FromBody] IReadOnlyCollection<Application.Dtos.Truck.CreateRequest> createRequests)
+    {
+        await administrator.CreateTrucks(createRequests);
+    }
+
+    [HttpPatch]
+    [Route("trucks")]
+    public async Task UpdateTrucks([FromBody] IReadOnlyCollection<Application.Dtos.Truck.UpdateRequest> updateRequests)
+    {
+        Console.WriteLine(JsonConvert.SerializeObject(updateRequests));
+        await administrator.UpdateTrucks(updateRequests);
     }
     
     [HttpDelete]
@@ -80,12 +69,37 @@ public class TestController(TransportCompanyContext dbContext, Administrator adm
     {
         await administrator.DeleteTrucks(FilterParser.Parse<Truck>(filter));
     }
-    
+
+    [HttpGet]
+    [Route("branches")]
+    public async Task<IActionResult> GetBranches()
+    {
+        return View("Branches",
+            (await administrator.GetBranches(FilterParser.Parse<Branch>("true"), true, true))
+            .Select(b => new ViewModels.Branch(b)).ToList());
+    }
+
+    [HttpPost]
+    [Route("branches")]
+    public async Task CreateBranches(
+        [FromBody] IReadOnlyCollection<Application.Dtos.Branch.CreateRequest> createRequests)
+    {
+        await administrator.CreateBranches(createRequests);
+    }
+
     [HttpPatch]
     [Route("branches")]
-    public async Task UpdateBranches([FromBody]IReadOnlyCollection<Application.Dtos.Branch.UpdateRequest> updateRequests)
+    public async Task UpdateBranches(
+        [FromBody] IReadOnlyCollection<Application.Dtos.Branch.UpdateRequest> updateRequests)
     {
         Console.WriteLine(JsonConvert.SerializeObject(updateRequests));
         await administrator.UpdateBranches(updateRequests);
+    }
+
+    [HttpDelete]
+    [Route("branches")]
+    public async Task DeleteBranches(string filter)
+    {
+        await administrator.DeleteBranches(FilterParser.Parse<Branch>(filter));
     }
 }
